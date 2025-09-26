90952586524abc68b49a680a93697ebf9769d77d
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-09-26 09:30:27 +0200

    feat: with consts

--- src/features/characters/services/fetch-characters.ts
+++ src/features/characters/services/fetch-characters.ts
@@ -1,8 +1,10 @@
 import { ApiListResponse } from "@/shared/contracts/api-list-response.dto";
+import { API_BASE_URL } from "@/shared/consts";
+
 import { Character } from "../contracts/product.dto";
 
 export const fetchCharacters = async () => {
-  const response = await fetch("https://rickandmortyapi.com/api/character");
+  const response = await fetch(`${API_BASE_URL}/character`);
   const data = (await response.json()) as ApiListResponse<Character>;
 
   return data;

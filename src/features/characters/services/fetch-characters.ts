d2d41f29afc57a99b9da3667e06da97ad851f883
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-09-26 09:38:16 +0200

    feat: with axios

--- src/features/characters/services/fetch-characters.ts
+++ src/features/characters/services/fetch-characters.ts
@@ -1,11 +1,25 @@
 import { ApiListResponse } from "@/shared/contracts/api-list-response.dto";
-import { API_BASE_URL } from "@/shared/consts";
+import { api } from "@/shared/api";
 
 import { Character } from "../contracts/product.dto";
 
-export const fetchCharacters = async () => {
-  const response = await fetch(`${API_BASE_URL}/character`);
-  const data = (await response.json()) as ApiListResponse<Character>;
+// export const fetchCharacters = async () => {
+//   const response = await fetch(`${API_BASE_URL}/character`);
+//   const data = (await response.json()) as ApiListResponse<Character>;
 
-  return data;
+//   return data;
+// };
+
+export const fetchCharacters = async () => {
+  const response = await api.get<ApiListResponse<Character[]>>("/character");
+
+  return response.data;
 };
+
+// export const createCharacters = async () => {
+//   const response = await api.post<ApiListResponse<Character[]>>("/character", {
+
+//   });
+
+//   return response.data;
+// };

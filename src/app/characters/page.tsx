4d1112ce4a70f36255cca3d65a7414cb89253035
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-10-03 09:54:48 +0200

    feat: with prisma client

--- src/app/characters/page.tsx
+++ src/app/characters/page.tsx
@@ -1,8 +1,11 @@
+// import { } from '@prisma/client';
+
 import { CharactersList } from "@/features/characters/components/CharactersList";
 import { fetchCharacters } from "@/features/characters/services/fetch-characters";
 
 export default async function CharactersPage() {
   const data = await fetchCharacters();
+  const records = 
 
   return (
     <div>

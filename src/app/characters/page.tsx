94dadb4972fbb62622ceec1eba179022e374c5e0
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-10-03 09:59:00 +0200

    feat: featching records

--- src/app/characters/page.tsx
+++ src/app/characters/page.tsx
@@ -1,16 +1,20 @@
-// import { } from '@prisma/client';
+import db from "@/shared/lib/db";
 
-import { CharactersList } from "@/features/characters/components/CharactersList";
-import { fetchCharacters } from "@/features/characters/services/fetch-characters";
+// import { CharactersList } from "@/features/characters/components/CharactersList";
+// import { fetchCharacters } from "@/features/characters/services/fetch-characters";
 
 export default async function CharactersPage() {
-  const data = await fetchCharacters();
-  const records = 
+  // const data = await fetchCharacters();
+  const records = await db.character.findMany();
 
   return (
     <div>
       <h2>Characters List</h2>
-      <CharactersList data={data.results} />
+      {/* <CharactersList data={data.results} /> */}
+
+      {records.map((elem) => (
+        <div key={elem.id}>{elem.name}</div>
+      ))}
     </div>
   );
 }

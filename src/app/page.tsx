2cfe8ae304d76248fe744a8afde4c7fe7087b504
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-10-24 09:47:59 +0200

    feat: with prisma adapter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
+import { auth } from "@/shared/lib/auth";
+import { headers } from "next/headers";
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  console.log({ session });
+
   return (
     <div>
       <nav>
--- src/shared/lib/auth.ts
+++ src/shared/lib/auth.ts
@@ -1,7 +1,14 @@
 import { betterAuth } from "better-auth";
 import { nextCookies } from "better-auth/next-js";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import db from "@/shared/lib/db";
 
 export const auth = betterAuth({
-  //...your config
+  emailAndPassword: {
+    enabled: true,
+  },
+  database: prismaAdapter(db, {
+    provider: "postgresql",
+  }),
   plugins: [nextCookies()], // make sure this is the last plugin in the array
 });
